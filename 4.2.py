7079c4a6c0bb015668dcea53513b4b579238bec9
MCUResource <rachelrapples@gmail.com>
2026-03-28 15:44:35 -0400

    Add files via upload

---
+++ 4.2.py
@@ -0,0 +1,11 @@
+small = True
+green = True
+
+if small and not green:
+    print("cherry")
+if small and green:
+    print("pea")
+if not small and green:
+    print("watermelon")
+if not small and not green:
+    print("pumpkin")
---
+++ 6.1.py
@@ -0,0 +1,2 @@
+for number in [3, 2, 1, 0]:
+    print(number)
